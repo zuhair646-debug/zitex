8e55df435a4b51107aadcca2bdabfb930e662992
zuhair646-debug <zuhair646@gmail.com>
2026-04-08 07:40:06 +0300

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -42,8 +42,10 @@ load_dotenv(ROOT_DIR / '.env')
 
 mongo_url = os.environ.get('MONGO_URL')
 db_name = os.environ.get('DB_NAME', 'zitex_db')
+
 # Disable TLS for Railway internal MongoDB
-if 'railway.internal' in str(mongo_url):
+
+  if 'railway.internal' in str(mongo_url):
     client = AsyncIOMotorClient(mongo_url, tls=False)
 else:
     client = AsyncIOMotorClient(mongo_url)
